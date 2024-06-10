cf1ec520ca69aab4ca1037045c62cb8419ae2caa
pramod51089 <127494282+pramod51089@users.noreply.github.com>
2024-06-10 18:23:22 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,29 +1,37 @@
-variable "subscriptions" {
-  description = "Map of subscription configurations"
-  type        = map(object({
-    endpoint                        = string
-    protocol                        = string
-    subscription_role_arn           = string
-    confirmation_timeout_in_minutes = number
-    delivery_policy                 = string
-    endpoint_auto_confirms         = bool
-    filter_policy                   = string
-    raw_message_delivery            = bool
-    filter_policy_scope             = string
-    redrive_policy                  = string
-  }))
+variable "role_arn" {
+    description = "The ARN of the role to use for Kinesis."
+    type        = string
 }
 
-variable "create_new_sns_topic" {
-  description = "Flag to indicate whether to create a new SNS topic"
-  type        = bool
-  default     = true
+variable "s3_bucket_arn" {
+    description = "The ARN of the S3 bucket for delivery stream."
+    type        = string
 }
 
-variable "create_subscription" {
-  description = "Flag to indicate whether to create subscription"
-  type        = bool
-  default     = true
+variable "name" {
+    description = "The name of the Kinesis stream."
+    type        = string
 }
 
+variable "enable_s3_logging" {
+    description = "Boolean to enable S3 logging."
+    type        = bool
+    default     = true  # Example default value
+}
 
+variable "cloudwatch_log_group_name" {
+    description = "The name of the CloudWatch log group for Kinesis logging."
+    type        = string
+}
+
+variable "s3_delivery_cloudwatch_log_stream_name" {
+    description = "The name of the CloudWatch log stream for S3 delivery."
+    type        = string
+}
+
+role_arn                              = "arn:aws:iam::123456789012:role/KinesisRole"
+s3_bucket_arn                         = "arn:aws:s3:::my-kinesis-bucket"
+name                                  = "my-kinesis-stream"
+enable_s3_logging                     = true
+cloudwatch_log_group_name             = "kinesis-log-group"
+s3_delivery_cloudwatch_log_stream_name = "s3-delivery-log-stream"
