38b1ebf0901cc6748ea0141c9971834c6256f581
pramod51089 <127494282+pramod51089@users.noreply.github.com>
2024-07-23 19:37:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-resource "aws_sns_topic_subscription" "this" {
-  for_each       = { for k, v in var.subscriptions : k => v if var.create_new_sns_topic && var.create_subscription && v.endpoint != null}
-  endpoint       = each.value.endpoint
-  protocol       = each.value.protocol
-  subscription_role_arn = each.value.protocol == "firehose" ? each.value.subscription_role_arn : null
-  topic_arn      = aws_sns_topic.this[0].arn
-  confirmation_timeout_in_minutes = lookup(each.value, "confirmation_timeout_in_minutes", null)
-  delivery_policy = lookup(each.value, "delivery_policy", null)
-  endpoint_auto_confirms = lookup(each.value, "endpoint_auto_confirms", false)
-  filter_policy  = lookup(each.value, "filter_policy", null)
-  raw_message_delivery = lookup(each.value, "raw_message_delivery", false)
-  filter_policy_scope = lookup(each.value, "filter_policy_scope", null)
-  redrive_policy = lookup(each.value, "redrive_policy", null)
-}
+Error: expected filter_policy_scope to be one of ["MessageAttributes" "MessageBody"], got 
+│
+│   with aws_sns_topic_subscription.this["subscription2"],
